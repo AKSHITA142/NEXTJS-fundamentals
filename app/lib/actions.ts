e067919089ae5b5aea9842d32e710fd25515f19e
Akshita Jariwala <akshitajariwala07@gmail.com>
2026-02-20 16:29:41 +0530

    Revalidate the path

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import {z} from 'zod';
 import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
 
 const sql=postgres(process.env.POSTGRES_URL!,{ssl: 'require'});
 
@@ -30,4 +31,5 @@ export async function createInvoice(formData: FormData){
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  revalidatePath('/dashboard/invoices');
 }
