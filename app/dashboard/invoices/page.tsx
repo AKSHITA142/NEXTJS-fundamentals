72abd095e6693e7d0a4463413ace54c4616aab5f
Akshita Jariwala <akshitajariwala07@gmail.com>
2026-02-21 13:55:59 +0530

    Wrapping the server actions into asyncronization

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,19 +6,20 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { Suspense } from 'react';
- 
+
 export default async function Page(
-  { searchParams }: {
-    searchParams?: {
+  props: {
+    searchParams?: Promise<{
       query?: string;
       page?: string;
-    };
+    }>;
   }
 ) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query ?? '';
-const currentPage = Number(searchParams?.page ?? '1');
-  const totalPages=await fetchInvoicesPages(query);
-    return (
+  const currentPage = Number(searchParams?.page ?? '1');
+  const totalPages = await fetchInvoicesPages(query);
+  return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
@@ -27,7 +28,7 @@ const currentPage = Number(searchParams?.page ?? '1');
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
