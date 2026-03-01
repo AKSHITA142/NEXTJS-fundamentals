a1a3299ee316b85711502a4b81b99054a531c773
Akshita Jariwala <akshitajariwala07@gmail.com>
2026-03-01 21:23:50 +0530

    Fix: Not found Page

--- app/dashboard/invoices/[id]/edit/not-found.tsx
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { FaceFrownIcon } from '@heroicons/react/24/outline';
  
+
+//Not found Page
 export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
